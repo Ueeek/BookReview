f556bbc3a8e9e2b79a0032acbd175ffd138ebf13
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-03-03 18:27:15 +0900

    fetch ranking

--- redux/actions/bookList.js
+++ redux/actions/bookList.js
@@ -1,13 +1,13 @@
-import { ADD_BOOK, DELETE_BOOK} from "../actionTypes";
+import { ADD_BOOKLIST, DELETE_BOOKLIST} from "../actionTypes";
 export const addBook = content => ({
-  type: ADD_BOOK,
+  type: ADD_BOOKLIST,
   payload: {
     content
   }
 });
 
 export const deleteBook = content => ({
-    type: DELETE_BOOK,
+    type: DELETE_BOOKLIST,
     payload: {
         content
     }
--- redux/reducers/bookList.js
+++ redux/reducers/bookList.js
@@ -1,12 +1,14 @@
-import { ADD_BOOK,GET_BOOKS,DELETE_BOOK} from "../actionTypes";
+import { ADD_BOOKLIST,FETCH_BOOKLIST,FETCH_BOOKLIST_FAILURE,FETCH_BOOKLIST_SUCCESS,DELETE_BOOKLIST} from "../actionTypes";
 const initialState = {
-    bookList: []
+    bookList: [],
+    loading:false,
+    error:"",
 }
 
 export default function(state = initialState, action) {
   console.log("book List=>",action.type)
   switch (action.type) {
-    case ADD_BOOK: {
+    case ADD_BOOKLIST: {
       const {content} = action.payload;
       alert("book:"+content.title+"is added to List!")
       return {
@@ -14,13 +16,30 @@ export default function(state = initialState, action) {
           bookList:[...state.bookList.filter((v,i)=>v!=content),content],
       };
     }
-      case DELETE_BOOK:{
+      case DELETE_BOOKLIST:{
           const {content} = action.payload;
           return{
               ...state,
               bookList:[...state.bookList.filter((v,i)=>v!=content)],
           };
       }
+    case FETCH_BOOKLIST:{
+        return{...initialState,...state,lloading:true}
+    }
+    case FETCH_BOOKLIST_SUCCESS:{
+        const {content} = action.payload;
+        return{
+            ...state,
+            bookList:{...initialState, bookList:content.payload}
+        }
+    }
+    case FETCH_BOOKLIST_FAILURE:{
+        const {content} = action.payload;
+        return{
+            ...initialState,error:content
+        }
+    }
+
     default:
           console.log("rank default called")
       return state;
